6a5fee46a6dcdd4bbd19c6e5935a4eed7c9fe90d
Wilson Aguilar <52500170+waguilars@users.noreply.github.com>
2021-11-30 13:46:53 -0500

    :bookmark: feat: app finished with friendly errors and default journal file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,38 @@
+use std::path::PathBuf;
+use anyhow::anyhow;
 use structopt::StructOpt;
+use tasks::Task;
+
+use crate::cli::{Action::*, CommandLineArgs};
 
 mod cli;
 mod tasks;
 
-fn main() {
-    let args = cli::CommandLineArgs::from_args();
-    println!("{:#?}", &args);
+fn main() -> anyhow::Result<()>{
+    // Get command line arguments
+    let CommandLineArgs {
+        action,
+        journal_file,
+    } = cli::CommandLineArgs::from_args();
+
+    // Unpack the journal file
+    let journal_file = journal_file
+        .or_else(find_default_journal_file)
+        .ok_or(anyhow!("Failed to find journal file."))?;
+
+    // Perform the action
+    match action {
+        Add { text } => tasks::add_task(journal_file, Task::new(text)),
+        List => tasks::list_tasks(journal_file),
+        Done { position } => tasks::complete_task(journal_file, position),
+    }?;
+
+    Ok(())
+}
+
+fn find_default_journal_file () -> Option<PathBuf> {
+    home::home_dir().map(|mut path| {
+        path.push(".rusty-journal.json");
+        path
+    })
 }
